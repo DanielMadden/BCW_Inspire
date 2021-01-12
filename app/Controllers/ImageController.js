8e14422001155d78abc55f67eaa823a16fec0dc5
Daniel Madden <danielfpjobs@gmail.com>
2021-01-12 16:49:51 -0700

    Updated readme; added comment in ImageController.

--- app/Controllers/ImageController.js
+++ app/Controllers/ImageController.js
@@ -8,6 +8,8 @@ import { localService } from "../Services/LocalService.js"
 // https://splashbase.s3.amazonaws.com/unsplash/regular/tumblr_n6rzpcsMk41st5lhmo1_1280.jpg
 // https://splashbase.s3.amazonaws.com/unsplash/regular/tumblr_mrran4OzSm1st5lhmo1_1280.jpg
 
+// GridSOM is usable by Vue, essentially fetches image during build process, loads 3 versions of it (different res). Loads lower right away, then slowly sharpens as page is ready.
+
 function _drawImage() {
     document.getElementById("background").style.background = `transparent url('${ProxyState.image}') no-repeat center center /cover`
 }
